2c1020c77fbb1797c052e5ece93eb437d832525e
MariuszWiacek <wiacekm.uber@gmail.com>
2023-03-12 07:23:47 +0000

    questions added

---
+++ assets/js/questions.js
@@ -0,0 +1,61 @@
+// Objects with question 
+let quizQuestions = [
+    
+    {
+        question: "1. What is first football clun of Christiano Ronaldo?",
+        answers: [
+            "1. Real Madrid",
+            "2. Manchester United",
+            "3. Sporting Lisbon",
+            "4. FC Porto"
+        ],
+    },
+
+    {
+        question: "2. Which year Lionel Messi scored 91 goals in one season?",
+        answers: [
+            "1. 2021",
+            "2. 2012",
+            "3. 2007",
+            "4. 2010"
+        ],
+    },
+
+    {
+        question: "3. Who won European Cup (currently Champions League) in 1991?",
+        answers: [
+            "1. Manchester United",
+            "2. FC Porto",
+            "3. Real Madrid",
+            "4. Crvena Zvezda Belgrade"
+        ],
+    },
+
+    {
+        question: "4. How many goals Robert Lewandowski scored in a champions league game in 2013 - Borussia Dortmund - Real Madrid (4-1)?",
+        answers: [
+            "1. 1",
+            "2. 2",
+            "3. 3",
+            "4. 4"
+        ],
+    },
+
+    {
+        question: "5. When was the fastest goal ever scored?",
+        answers: [
+            "1. In 12 sec of a game",
+            "2. In 4.1 sec of a game",
+            "3. In 1.5 sec of a game",
+            "4. In 2.4 sec of a game"
+        ],
+    },
+]
+
+//Variables with correct answers
+
+let correctAnswer1 = "3. 2007"
+let correctAnswer2 = "2. 2012"
+let correctAnswer3 = "4. Crvena Zvezda Belgrade"
+let correctAnswer4 = "4. 4"
+let correctAnswer5 = "4. 2.4"
